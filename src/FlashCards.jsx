5b61b250f70f3529719a5f0f4994dbdd2d31e7c4
Kundan Tripathi <124955318+kundantripathi98@users.noreply.github.com>
2024-06-12 17:43:36 +0530

    Initial commit

---
+++ src/FlashCards.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { questions } from "./App";
+
+function FlashCards() {
+  //   const [titles, setTitles] = useState({});
+  const [selectedId, setSelectedId] = useState(null);
+
+  const handleClick = (id, answer) => {
+    // setTitles((prevTitles) => ({
+    //   [id]: answer,
+    // }));
+
+    setSelectedId(id);
+  };
+
+  return (
+    <div className="flashcards">
+      {questions.map((item) => (
+        <div
+          key={item.id}
+          className={selectedId === item.id ? "selected" : ""}
+          onClick={() => handleClick(item.id, item.answer)}
+        >
+          {selectedId === item.id ? item.answer : item.question}
+          {/* {titles[item.id] || item.question} */}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default FlashCards;
